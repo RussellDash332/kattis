b25cebd38c8c7d60ad310d520bf46a2901c2a99c
RussellDash332 <russellsaerang@gmail.com>
2021-02-02 15:50:05 +0800

    Added Quadrant Selection

---
+++ Quadrant Selection/quadrant-sm.js
@@ -0,0 +1,11 @@
+var x = readline();
+var y = readline();
+if (x > 0 && y > 0) {
+    console.log(1);
+} else if (y > 0) {
+    console.log(2);
+} else if (x > 0) {
+    console.log(4);
+} else {
+    console.log(3);
+}
---
+++ Quadrant Selection/quadrant.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+if x>0 and y>0:
+    print(1)
+elif y>0:
+    print(2)
+elif x>0:
+    print(4)
+else:
+    print(3)
