6133066edd8414e8f5f5ba24deff2cdae6d7007b
RussellDash332 <russellsaerang@gmail.com>
2021-02-09 22:43:29 +0800

    Update as of 9 February 2021

--- src/Apaxiaaans/apaxiaaans-sm.js
+++
@@ -1,8 +0,0 @@
-let line = readline();
-let result = line[0];
-for (let i = 1; i < line.length; i++) {
-    if (result[result.length-1] !== line[i]) {
-        result += line[i]
-    }
-}
-console.log(result);
--- src/Apaxiaaans/apaxiaaans.py
+++
@@ -1,6 +0,0 @@
-name = input()
-result = name[0]
-for k in name[1:]:
-    if result[-1] != k:
-        result += k
-print(result)
