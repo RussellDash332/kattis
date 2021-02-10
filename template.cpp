1d1e2f5c2130427b9b30e5a7cd218beddc3b0b64
RussellDash332 <russellsaerang@gmail.com>
2021-02-10 15:57:41 +0800

    Added template for C++

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h> // for math functions
+#include <string> // for string functions
+using namespace std;
+
+int main()
+{
+    int n;
+    int ans;
+    cin >> n;
+    cout << ans;
+    return 0;
+}
