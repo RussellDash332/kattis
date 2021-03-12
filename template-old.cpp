5c7d57aea2c5084f07343e551e8fce3326aba5f5
RussellDash332 <russellsaerang@gmail.com>
2021-03-12 18:44:42 +0800

    Minor updates

---
+++ template-old.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h> // for math functions
+#include <string> // for string functions
+#include <sstream> // for stringstream
+#include <algorithm> // for finding functions
+#include <stack> // for stack implementations
+#include <set> // for set implementations
+#include <map> // for map implementations
+#include <vector> // for vector implementations
+#include <utility> // for pair implementations
+#include <iomanip> // for precision in cout
+using namespace std;
+
+int main()
+{
+    int n, ans;
+    cin >> n;
+
+    while (n--) {
+        // one time use of n, e.g. number of test cases
+        int x;
+        cin >> x;
+    }
+
+    cout << ans;
+    return 0;
+}
