4763a0029771a5dbf4f84d1dcb4128769fa60f1c
RussellDash332 <russellsaerang@gmail.com>
2022-03-19 23:21:13 +0800

    Update Tug of War

--- src/Tug of War/tugofwar.py
+++ src/Tug of War/tugofwar.py
@@ -6,25 +6,22 @@ for line in sys.stdin:
     p.append(int(line))
 s = sum(p)
 
-# dp[i][j] = True if I can assign i people with total sum j on a team, then check for i = n // 2 only
+# j in dp[i] if I can assign i people with total sum j on a team, then check for i = n // 2 only
 dp = []
 for _ in range(n // 2 + 1):
-    dp2 = []
-    for _ in range(s + 1):
-        dp2.append(False)
-    dp.append(dp2)
+    dp.append(set())
 
-dp[0][0] = True
+dp[0].add(0)
 # Check for every person given an initial configuration
 for w in p:
     for i in range(n // 2 - 1, -1, -1):
-        for j in range(s - w, -1, -1):
-            if dp[i][j]:
-                dp[i + 1][j + w] = True
+        for j in dp[i]:
+            if j <= s - w:
+                dp[i + 1].add(j + w)
 
 left, diff = 0, s
-for tw in range(s + 1):
-    if dp[n // 2][tw] and abs(s - 2 * tw) < diff:
+for tw in dp[n // 2]:
+    if abs(s - 2 * tw) < diff:
         left, diff = tw, abs(s - 2 * tw)
 if left > s - left:
     left = s - left
