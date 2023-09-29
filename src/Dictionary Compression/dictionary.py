ed131db646bfb9edd1b12b5b38776564de2ef4fe
RussellDash332 <russellsaerang@gmail.com>
2023-09-30 03:43:53 +0800

    Update as of 30 September 2023

--- src/Dictionary Compression/dictionary.py
+++ src/Dictionary Compression/dictionary.py
@@ -1,11 +1,4 @@
-def d(s):
- s = s.replace('^', ':{')
- for i in range(17,0,-1): s = s.replace(str(i),'}'*i)
- return eval(s.replace('#','{.:0').replace('{',"{'").replace(':',"':").replace(',',",'").replace("'",'"').replace('@',"'").replace('!',"'s"))
-def f(t,s=[],w=''):
- for k,v in t.items():
-  if k=='.':s.append(w)
-  else:f(v,s,w+k)
-def m(p):s=[];f(d(open(p).read()),s);[*map(print,sorted(s))]
-try:m('/src/cc')
-except:m('./src/cc')
+# python comp.py | bzip2 > dictionary && python dictionary.py
+import bz2, os; w=b''
+#print(os.path.getsize('dictionary'))
+for x in bz2.open(__file__[:-3]).read().split(b'%'):print((w:=(x and w[:len(w)-x[0]]+x[1:])or w+b"'s").decode())
